c0f9e547029f47370bdadb76234a2d2a657b72d1
huangxs48 <xh2pm@virginia.edu>
2020-04-29 10:23:49 -0400

    user hst header

--- code/src/outputs/history.cpp
+++ code/src/outputs/history.cpp
@@ -220,7 +220,7 @@ void HistoryOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
         std::fprintf(pfile,"[%d]=%d-scalar    ", iout++, n);
       }
       for (int n=0; n<pm->nuser_history_output_; n++)
-        std::fprintf(pfile,"[%d]=%-8s", iout++,
+        std::fprintf(pfile,"[%d]=%-8s    ", iout++,
                      pm->user_history_output_names_[n].c_str());
       std::fprintf(pfile,"\n");                              // terminate line
     }
